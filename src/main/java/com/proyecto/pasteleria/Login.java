2e44a14ccdb54b61eddac2e9127abb25ca160f97
Pasteleria_App <https://github.com/diego178/pasteleria_app.git>
2022-05-15 00:03:17 -0500

    First version

---
+++ src/main/java/com/proyecto/pasteleria/Login.java
@@ -0,0 +1,7 @@
+package com.proyecto.pasteleria;
+
+import javafx.scene.layout.BorderPane;
+
+public class Login extends BorderPane {
+
+}
---
+++ src/main/java/com/proyecto/pasteleria/Main.java
@@ -0,0 +1,25 @@
+package com.proyecto.pasteleria;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane root = new Login();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
